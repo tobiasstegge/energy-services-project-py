38d196d9b7abe4cd245df76d33d693a70ac23336
tobiassteggemann <tobias.steggemann@t-online.de>
2022-04-01 13:03:51 +0100

    finish commenting

--- src/ds_tools/time_series_evaluation.py
+++ src/ds_tools/time_series_evaluation.py
@@ -5,7 +5,7 @@ from math import sqrt
 
 def evaluate_forecasts(actual, predicted):
     errors = {}
-    errors['msa'] = mean_absolute_error(y_true=actual, y_pred=predicted)
+    errors['mae'] = mean_absolute_error(y_true=actual, y_pred=predicted)
     errors['mse'] = mean_squared_error(y_true=actual, y_pred=predicted)
     errors['rmse'] = sqrt(mean_squared_error(y_true=actual, y_pred=predicted))
     errors['mape'] = mean_absolute_percentage_error(y_true=actual, y_pred=predicted)
